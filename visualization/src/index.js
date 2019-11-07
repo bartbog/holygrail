d7fb61afcf3a29ae0f83d3613846bc3d099b831f
Bart Bogaerts <bart.bogaerts@cs.kuleuven.be>
2019-11-07 06:39:08 +0100

    pasta

--- visualization/src/index.js
+++ visualization/src/index.js
@@ -8,7 +8,7 @@ import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.js';
 
-const problemName = 'p5-split';
+const problemName = 'nielspasta-split';
 
 ReactDOM.render( <PuzzleInfo /> , document.getElementById('problemIntro'));
 ReactDOM.render( <App problemName={problemName} /> , document.getElementById('root'));

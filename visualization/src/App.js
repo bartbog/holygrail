0f5272bccee6a43b67d7d8004e63ee144d67f442
Emilio <emilio.gamba@outlook.be>
2020-02-28 15:04:23 +0100

    correct explanation

--- visualization/src/App.js
+++ visualization/src/App.js
@@ -27,37 +27,38 @@ const initialState = {
   assumptions: []
 };
 
+function nestedReducer(state, next){
+  return {
+    clue: next.clue,
+    known: [...state.known, ...state.derived],
+    derived: next.derivations,
+    assumptions: next.assumptions,
+  };
+}   
+
+function sequenceReduce(state, next){
+
+  const nestedSequenceInitialState = {
+    clue: null,
+    known: state.known == null ? [] : state.known,
+    derived: [],
+    assumptions: []
+  }
+
+  return {
+    fact: next.derivable_fact,
+    known:state.known,
+    reason_sequence: next.reason_sequence != null ? R.scan(nestedReducer, nestedSequenceInitialState, next.reason_sequence).slice(1) : null
+  }
+}  
+
 function reducer(state, next) {
 
   const reasonSequenceInitialState = {
     fact:null,
-    reason_sequence: []
+    reason_sequence: [],
+    known: next.assumptions
   } 
-
-  function sequenceReduce(state, next){
-
-    const nestedSequenceInitialState = {
-      clue: null,
-      known: [],//state.known == null ? [] : state.known,
-      derived: [],
-      assumptions: []
-    }
-
-    function nestedReducer(state, next){
-      return {
-        clue: next.clue,
-        known: [...state.known, ...state.derived],
-        derived: next.derivations,
-        assumptions: next.assumptions,
-      };
-    }    
-  
-    return {
-      fact: next.derivable_fact,
-      // known:state.known,
-      reason_sequence: next.reason_sequence != null ? R.scan(nestedReducer, nestedSequenceInitialState, next.reason_sequence).slice(1) : null
-    }
-  }  
 
   return {
     clue: next.clue,
@@ -289,11 +290,14 @@ function App({ problemName }) {
     const nested_explanations = facts.nested_explanations.filter((nExpl) => nExpl.fact != null)
     
     if(sequenceIndex > 0 && nestedIndex > 0){
+      console.log("Here")
+      console.log(facts)
+
       counterfact = nested_explanations[sequenceIndex-1].fact
 
       facts=nested_explanations[sequenceIndex-1].reason_sequence.filter((nExpl) => nExpl.clue != null)[nestedIndex-1]
       const nested_sequence_length = nested_explanations[sequenceIndex-1].reason_sequence.length
-
+      console.log(facts)
       prevNextButton = <div>
           <table>
           <td><button onClick={() => setIndexClipped(index - 1)}>&#8592; Prev</button></td>
@@ -438,38 +442,49 @@ function RegularExplanation({ type1, type2, facts, counterfact }){
             );
 
             let counterfactColored = null
+
             if(counterfact != null){
               counterfactColored = getKnowledgeFrom(
                 [counterfact],
                 entity1,
                 entity2
               );
-              
             }
 
-            const knowledge =
-              derivedKnowledge || assumedKnowledge || knownKnowledge|| counterfactColored;
+            const knowledge = derivedKnowledge || assumedKnowledge || knownKnowledge || counterfactColored;
+
             let color = null;
             let frontcolor = "#000";
 
-            if(counterfactColored != null){
-              color = "lightcoral"
-            }else 
-            if (derivedKnowledge != null) {
-              color = knowledge.value ? "#FF6600" : "#FF6600";
-            }else if (assumedKnowledge != null) {
+            // if(counterfactColored != null){
+            //   color = "lightcoral"
+            // }else 
 
-              if(facts.derived[0] === "UNSAT"){
-                color ="red"
-              }else{
-                color = "#003399"; //Asymmetry true/false is not so important here...
-                frontcolor = "white";
+            if(counterfactColored!=null && facts.derived[0] === "UNSAT"){
+              color = "red"
+            }else if(derivedKnowledge != null) {
+                color = "#FF6600";
+            }else if(assumedKnowledge != null) {
+              color = "#003399"; //Asymmetry true/false is not so important here...
+              frontcolor = "white";
+            }else if (knowledge != null) {
+                color = "whitesmoke";
               }
+            // if (derivedKnowledge != null) {
+            //   color = "#FF6600";
+            // }else if (assumedKnowledge != null) {
+            //   if(facts.derived[0] === "UNSAT" && counterfactColored != null){
+            //     color ="red"
+            //   }
+            //   else{
+            //     color = "#003399"; //Asymmetry true/false is not so important here...
+            //     frontcolor = "white";
+            //   }
 
               
-            } else if (knowledge != null) {
-              color = "whitesmoke";
-            }
+            // } else if (knowledge != null) {
+            //   color = "whitesmoke";
+            // }
 
             return (
               <div style={styles.childFillGridItem(color, frontcolor)}>
@@ -498,11 +513,12 @@ function SequenceExplanationGrid({ type1, type2, derived, known, funSequenceInde
             );
             const posInDerived = derived.indexOf(factKnowledge) +1
             
-            const knownKnowledge = getKnowledgeFrom(
-              known,
-              entity1,
-              entity2
-            );
+            const knownKnowledge = null
+            // getKnowledgeFrom(
+            //   known,
+            //   entity1,
+            //   entity2
+            // );
 
             const knowledge = factKnowledge || knownKnowledge;
 

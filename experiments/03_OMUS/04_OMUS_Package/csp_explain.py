56f415efe748933d5da4eb23b90b987cf667878e
Emilio <emilio.gamba@outlook.be>
2020-08-27 19:25:25 +0200

    correction bug

--- experiments/03_OMUS/04_OMUS_Package/csp_explain.py
+++ experiments/03_OMUS/04_OMUS_Package/csp_explain.py
@@ -7,8 +7,8 @@ import random
 from pysat.formula import CNF
 from pysat.solvers import Solver
 
-sys.path.append('/home/crunchmonster/Documents/VUB/01_SharedProjects/01_cppy_src')
-# sys.path.append('/home/emilio/Documents/cppy/')
+# sys.path.append('/home/crunchmonster/Documents/VUB/01_SharedProjects/01_cppy_src')
+sys.path.append('/home/emilio/Documents/cppy_src/')
 
 from cppy import BoolVarImpl, Comparison, Model, Operator, cnf_to_pysat
 # from cppy.solver_interfaces.pysat_tools import 

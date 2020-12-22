3585805a1de7d511fd88a77beec718e8b9047587
Emilio <emilio.gamba@outlook.be>
2020-12-22 14:20:15 +0100

    will it work ?

--- experiments/03_OMUS/02_OUS/experiment_rq1.py
+++ experiments/03_OMUS/02_OUS/experiment_rq1.py
@@ -27,6 +27,7 @@ def r_frietkotProblem(params):
     f = cost(U, I)
     explain(C=frietkot_cnf, U=U, f=f, I0=I, params=params, verbose=True)
 
+
 # @ray.remote
 def r_originProblem(params):
     params.instance = "origin-problem"
@@ -36,6 +37,7 @@ def r_originProblem(params):
     I = set(x for lst in o_assumptions for x in lst)
     f = cost_puzzle(U, I, o_weights)
     explain(C=o_cnf, U=U, f=f, I0=I, params=params, verbose=False)
+
 
 # @ray.remote
 def r_simpleProblem(params):
@@ -104,19 +106,21 @@ def rq1_maxsat_grow():
             all_exec_params.append(p)
 
     runParallel([r_originProblem], all_exec_params)
-    # return all_exec_params
 
 
 def rq1_all_params():
-
     # checking the effect on preseeding with grow
+    polarity = True
+    preseeding = True
+    preseedingGrow = True
     TF = [True, False]
     all_params_test = []
 
-    pre_grow_perms = []
-    for c in [list(per) for per in itertools.permutations([True, False, False])]:
-        if c not in pre_grow_perms:
-            pre_grow_perms.append(c)
+    # pre_grow_perms = []
+    # for c in [list(per) for per in itertools.permutations([True, False])]:
+    #     if c not in pre_grow_perms:
+    #         pre_grow_perms.append(c)
+
     grow_perms = []
     for c in [[True] + list(per) for per in itertools.permutations([True, False, False])] + [[False] * 4]:
         if c not in grow_perms:
@@ -127,82 +131,80 @@ def rq1_all_params():
         if c not in grow_maxsat_perms:
             grow_maxsat_perms.append(c)
 
-    for pre_grow, pre_subset, pre_maxsat in pre_grow_perms:
-        for postopt, post_opt_greedy in itertools.product(TF, repeat=2):
-            for grow, grow_sat, grow_subset, grow_maxsat in grow_perms:
-                grow_maxsat_neg_cost, grow_maxsat_pos_cost, grow_maxsat_max_cost_neg, grow_maxsat_unit = False, False, False, False
-                if grow_maxsat:
-                    for grow_maxsat_neg_cost, grow_maxsat_pos_cost, grow_maxsat_max_cost_neg, grow_maxsat_unit in grow_maxsat_perms:
-                        p = COusParams()
+    postponeOpt_perms = [
+        [False, False, False],
+        [True, False, True],
+        [True, True, False],
+        [True, True, True]
+    ]
 
-                        # polarity
-                        p.polarity = True
-
-                        # pre-seeding
-                        p.pre_seeding = True
-                        p.pre_seeding_grow = pre_subset
-                        p.pre_seeding_subset_minimal = pre_grow
-                        p.pre_seeding_grow_maxsat = pre_maxsat
-
-                        # hitting set computation
-                        p.postpone_opt = (postopt or post_opt_greedy)
-                        p.postpone_opt_incr = postopt
-                        p.postpone_opt_greedy = post_opt_greedy
-
-                        # grow
-                        p.grow = grow
-                        p.grow_sat = grow_sat
-                        p.grow_subset_maximal = grow_subset
-                        p.grow_maxsat = grow_maxsat
-
-
-                        p.grow_maxsat_neg_cost = grow_maxsat_neg_cost
-                        p.grow_maxsat_pos_cost = grow_maxsat_pos_cost
-                        p.grow_maxsat_max_cost_neg = grow_maxsat_max_cost_neg
-                        p.grow_maxsat_unit = grow_maxsat_unit
-
-                        p.timeout = 4 * HOURS
-                        p.output_folder = "/user/brussel/101/vsc10143/holygrail/experiments/03_OMUS/02_OUS/results/rq1_5/" + datetime.now().strftime("%Y%m%d%H") + "/"
-                        all_params_test.append(p)
-                else:
+    for postopt, postponeIncr, postponeGreedy in postponeOpt_perms:
+        for grow, growSat, growSubset, growMaxsat in grow_perms:
+            if growMaxsat:
+                for maxsatNegCost, maxsatPosCost, maxsatMaxNegCost, maxsatUnit in grow_maxsat_perms:
                     p = COusParams()
 
                     # polarity
-                    p.polarity = True
+                    p.polarity = polarity
 
                     # pre-seeding
-                    p.pre_seeding = True
-                    p.pre_seeding_grow = pre_subset
-                    p.pre_seeding_subset_minimal = pre_grow
-                    p.pre_seeding_grow_maxsat = pre_maxsat
+                    p.pre_seeding = preseeding
+                    p.pre_seeding_grow = preseedingGrow
 
                     # hitting set computation
-                    p.postpone_opt = (postopt or post_opt_greedy)
-                    p.postpone_opt_incr = postopt
-                    p.postpone_opt_greedy = post_opt_greedy
+                    p.postpone_opt = postopt
+                    p.postpone_opt_incr = postponeIncr
+                    p.postpone_opt_greedy = postponeGreedy
 
                     # grow
                     p.grow = grow
-                    p.grow_sat = grow_sat
-                    p.grow_subset_maximal = grow_subset
-                    p.grow_maxsat = grow_maxsat
+                    p.grow_sat = growSat
+                    p.grow_subset_maximal = growSubset
+                    p.grow_maxsat = growMaxsat
 
-
-                    p.grow_maxsat_neg_cost = grow_maxsat_neg_cost
-                    p.grow_maxsat_pos_cost = grow_maxsat_pos_cost
-                    p.grow_maxsat_max_cost_neg = grow_maxsat_max_cost_neg
-                    p.grow_maxsat_unit = grow_maxsat_unit
+                    p.grow_maxsat_neg_cost = maxsatNegCost
+                    p.grow_maxsat_pos_cost = maxsatPosCost
+                    p.grow_maxsat_max_cost_neg = maxsatMaxNegCost
+                    p.grow_maxsat_unit = maxsatUnit
 
                     p.timeout = 4 * HOURS
-                    p.output_folder = "/user/brussel/101/vsc10143/holygrail/experiments/03_OMUS/02_OUS/results/rq1_5/" + datetime.now().strftime("%Y%m%d%H") + "/"
+                    p.output_folder = "/user/brussel/101/vsc10143/holygrail/experiments/03_OMUS/02_OUS/results/rq1_6/" + datetime.now().strftime("%Y%m%d%H") + "/"
                     all_params_test.append(p)
+            else:
+                p = COusParams()
+
+                # polarity
+                p.polarity = polarity
+
+                # pre-seeding
+                p.pre_seeding = preseeding
+                p.pre_seeding_grow = preseedingGrow
+
+                # hitting set computation
+                p.postpone_opt = postopt
+                p.postpone_opt_incr = postponeIncr
+                p.postpone_opt_greedy = postponeGreedy
+
+                # grow
+                p.grow = grow
+                p.grow_sat = growSat
+                p.grow_subset_maximal = growSubset
+                p.grow_maxsat = growMaxsat
+
+                p.grow_maxsat_neg_cost = False
+                p.grow_maxsat_pos_cost = False
+                p.grow_maxsat_max_cost_neg = False
+                p.grow_maxsat_unit = False
+
+                p.timeout = 4 * HOURS
+                p.output_folder = "/user/brussel/101/vsc10143/holygrail/experiments/03_OMUS/02_OUS/results/rq1_6/" + datetime.now().strftime("%Y%m%d%H") + "/"
+                all_params_test.append(p)
 
     return all_params_test
 
 
 def rq2_params():
     pass
-
 
 # def rq1():
 #     ray.init(address='auto')
@@ -212,8 +214,16 @@ def rq2_params():
 #     ray.get(futures)
 
 
+def rq1_args(func, num_node, cores_per_node):
+    offset = (num_node-1)*cores_per_node
+    params = rq1_all_params()[offset:]
+    runParallel([func], params)
+
+
 if __name__ == "__main__":
-    # rq1()
-    # maxSatGrowparams = rq1_maxsat_grow()
-    # print(len(maxSatGrowparams))
-    rq1_maxsat_grow()
+    if len(sys.argv) == 3:
+        rq1_args(r_originProblem, int(sys.argv[1]), int(sys.argv[2]))
+    else:
+        print ('Number of arguments:', len(sys.argv), 'arguments.')
+        print ('Argument List:', str(sys.argv))
+        print(len(rq1_all_params()))

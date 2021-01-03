b64bd1470d6955d6f5ca6f879438434bf91e4fd9
Emilio <emilio.gamba@outlook.be>
2021-01-03 09:24:40 +0100

    Pasta puzzle implemented ?

--- experiments/03_OMUS/02_OUS/frietkot.py
+++ experiments/03_OMUS/02_OUS/frietkot.py
@@ -52,8 +52,8 @@ def pastaPuzzle():
     """
     dollar = ['4', '8', '12', '16']
     person = ['angie', 'damon', 'claudia', 'elisa']
-    sauce = ['the_other_type1', 'arrabiata_sauce', 'marinara_sauce', 'puttanesca_sauce'] 
-    pasta = ['capellini', 'farfalle', 'tagliolini', 'rotini']
+    sauce = ['the_other_type1', 'arrabiata_sauce', 'marinara_sauce', 'puttanesca_sauce'] # type1
+    pasta = ['capellini', 'farfalle', 'tagliolini', 'rotini']  # type2
 
     types = [dollar, person, sauce, pasta]
     n = len(types)
@@ -63,12 +63,228 @@ def pastaPuzzle():
     chose = Relation(person, sauce)
     paid = Relation(person, dollar)
     ordered = Relation(person, pasta)
-    # is_linked_with_1(sauce, dollar)
-    # is_linked_with_2(sauce, pasta)
-    # is_linked_with_3(dollar, pasta)
+    sauce_dollar = Relation(sauce, dollar) # is_linked_with_1(sauce, dollar)
+    sauce_pasta = Relation(sauce, pasta) # is_linked_with_2(sauce, pasta)
+    dollar_pasta = Relation(dollar, pasta) # is_linked_with_3(dollar, pasta)
+    rels = [chose, paid, ordered, sauce_dollar, sauce_pasta, dollar_pasta]
+
+    # Bijectivity
+    cnt = 0
+    bij = []
+    bv_bij = []
+
+    for rel in rels:
+        # for each relation
+        for col_ids in rel.df:
+            bv1 = BoolVar()
+            bv2 = BoolVar()
+            # one per column
+            atleast, atmost = exactly_one_at_most(rel[:, col_ids])
+            [bij.append(implies(bv1, clause)) for clause in atmost]
+            bij.append(implies(bv2, atleast))
+            bv_bij.append(bv1)
+            bv_bij.append(bv2)
+
+        for (_,row) in rel.df.iterrows():
+            bv3 = BoolVar()
+            bv4 = BoolVar()
+            # one per row
+            atleast, atmost = exactly_one_at_most(row)
+            [bij.append(implies(bv3, clause)) for clause in atmost]
+            bij.append(implies(bv4, atleast))
+            bv_bij.append(bv3)
+            bv_bij.append(bv4)
+
+    # Transitivity
+    trans = []
+    bv_trans =  [BoolVar() for i in range(12)]
+
+    for x in person:
+        for y in sauce:
+            for z in dollar:
+                t0 = to_cnf(implies(paid[x, z] & sauce_dollar[y, z], chose[x, y]))
+                [trans.append(implies(bv_trans[0], clause)) for clause in t0]
+
+                t1 = to_cnf(implies(~paid[x, z] & sauce_dollar[y, z], ~chose[x, y]))
+                [trans.append(implies(bv_trans[1], clause)) for clause in t1]
+
+                t2 = to_cnf(implies(paid[x, z] & ~sauce_dollar[y, z], ~chose[x, y]))
+                [trans.append(implies(bv_trans[2], clause)) for clause in t2]
+
+    for x in person:
+        for y in sauce:
+            for z in pasta:
+                t3 = to_cnf(implies(ordered[x, z] & sauce_pasta[y, z], chose[x, y]))
+                [trans.append(implies(bv_trans[3], clause)) for clause in t3]
+
+                t4 = to_cnf(implies(~ordered[x, z] & sauce_pasta[y, z], ~chose[x, y]))
+                [trans.append(implies(bv_trans[4], clause)) for clause in t4]
+
+                t5 = to_cnf(implies(ordered[x, z] & ~sauce_pasta[y, z], ~chose[x, y]))
+                [trans.append(implies(bv_trans[5], clause)) for clause in t5]
+
+    for x in person:
+        for y in dollar:
+            for z in pasta:
+                t6 = to_cnf(implies(ordered[x, z] & dollar_pasta[y, z], paid[x, y]))
+                [trans.append(implies(bv_trans[6], clause)) for clause in t6]
+
+                t7 = to_cnf(implies(~ordered[x, z] & dollar_pasta[y, z], ~paid[x, y]))
+                [trans.append(implies(bv_trans[7], clause)) for clause in t7]
+
+                t8 = to_cnf(implies(ordered[x, z] & ~dollar_pasta[y, z], ~paid[x, y]))
+                [trans.append(implies(bv_trans[8], clause)) for clause in t8]
 
 
-    return
+    for x in sauce:
+        for y in dollar:
+            for z in pasta:
+                t9 = to_cnf(implies(sauce_pasta[x, z] & dollar_pasta[y, z], sauce_dollar[x, y]))
+                [trans.append(implies(bv_trans[9], clause)) for clause in t9]
+
+                t10 = to_cnf(implies(~sauce_pasta[x, z] & dollar_pasta[y, z], ~sauce_dollar[x, y]))
+                [trans.append(implies(bv_trans[10], clause)) for clause in t10]
+
+                t11 = to_cnf(implies(sauce_pasta[x, z] & ~dollar_pasta[y, z], ~sauce_dollar[x, y]))
+                [trans.append(implies(bv_trans[11], clause)) for clause in t11]
+
+    clues = []
+    bv_clues = [BoolVar() for i in range(8)]
+
+    # type1 = sauce
+    # person
+    # type2 = pasta
+    # type3 = differences between values of type dollar
+    # type4 = differences between values of type dollar
+    # type5 = differences between values of type dollar
+
+    # 0.The person who ordered capellini paid less than the person who chose arrabiata sauce
+    # assumption_satisfied( 0  ) => ?a [person] b [type3] c [dollar] d [person] e [dollar]: ordered(a,capellini) & b>0 & chose(d,arrabiata_sauce) & paid(d,c) & e = c-b & paid(a,e).
+    c0a = []
+    for a in person:
+        # b > 0
+        for b in [4, 8, 12]:
+            for c in dollar:
+                for d in person:
+                    for e in dollar:
+                        if e == c-b:
+                            c0a.append(ordered[a, "capellini"] & chose[d, "arrabiata_sauce"] & paid[d, str(c)] & paid[a, str(e)])
+    [clues.append(implies(bv_clues[0], clause)) for clause in c0a]
+
+    # 1. The person who chose arrabiata sauce ordered farfalle
+    # assumption_satisfied( 0  ) => ?f [person]: chose(f,arrabiata_sauce) & ordered(f,farfalle).
+    c1a = to_cnf([implies(chose[p, "arrabiata_sauce"], ordered[p, "farfalle"]) for p in person])
+    [clues.append(implies(bv_clues[1], clause)) for clause in c1a]
+
+    # 2. The person who ordered tagliolini paid less than the person who chose marinara sauce
+    # assumption_satisfied( 0  ) => ?g [person] h [type4] i [dollar] j [person] k [dollar]: ordered(g,tagliolini) & h>0 & chose(j,marinara_sauce) & paid(j,i) & k = i-h & paid(g,k).
+    c2a = []
+    for g in person:
+        # h > 0
+        for h in [4, 8, 12]:
+            for i in dollar:
+                for j in person:
+                    for k in dollar:
+                        if k == i - h:
+                            c2a.append(ordered[g, "taglioni"] & chose[j, "marinara_sauce"] & paid[str(j), str(i)] & paid[g, str(k)])
+    [clues.append(implies(bv_clues[2], clause)) for clause in c2a]
+
+    #  3. The person who ordered tagliolini paid more than Angie
+    # assumption_satisfied( 0  ) => ?l [person] m [type5] n [dollar] o [dollar]: ordered(l,tagliolini) & m>0 & paid(angie,n) & o = n+m & paid(l,o).
+    c3a = []
+    for p in person:
+        for o in dollar:
+            for n in [-4, 4, -8, 8, -12, 12]:
+                for m in [4, 8, 12]:
+                    if o == n + m:
+                        c3a.append(ordered[p, "taglioni"] & paid["angie", str(n)] & paid[p, str(o)])
+    [clues.append(implies(bv_clues[3], clause)) for clause in c3a]
+
+    #  4. The person who ordered rotini is either the person who paid $8 more than Damon or the person who paid $8 less than Damon
+    # assumption_satisfied( 0  ) => ?
+    # p [person]: ordered(p,rotini) & ((?q [person] r [dollar] s [dollar]: paid(damon,r) & s = r+8 & paid(q,s) & q = p) | (?t [person] u [dollar] v [dollar]: paid(damon,u) & v = u-8 & paid(t,v) & t = p)).
+    c4a = []
+    for p in person:
+        for r in dollar:
+            for s in dollar:
+                if s == r + 8 or s == r - 8:
+                    c4a.append(
+                        implies(
+                            ordered[p, "rotini"],
+                            paid["damon", str(r)] & paid[p, str(s)]
+                        )
+                    )
+    [clues.append(implies(bv_clues[4], clause)) for clause in c4a]
+
+
+    # 5. Claudia did not choose puttanesca sauce
+    # assumption_satisfied( 0  ) => ~ chose(claudia,puttanesca_sauce).
+    clues.append(implies(bv_clues[5],  ~chose["claudia", "puttanesca_sauce"]))
+
+    #  6. The person who ordered capellini is either Damon or Claudia
+    # assumption_satisfied( 0  ) => ?w [person]: ordered(w,capellini) & (damon = w | claudia = w).
+    clues.append(implies(bv_clues[6],  (ordered["claudia", "capellini"] & ~ordered["damon", "capellini"]) | (~ordered["claudia", "capellini"] & ordered["damon" , "capellini"])))
+
+    # 7. The person who chose arrabiata sauce is either Angie or Elisa => XOR
+    # assumption_satisfied( 0  ) => ?x [person]: chose(x,arrabiata_sauce) & (angie = x | elisa = x).
+    clues.append(
+        implies(
+            bv_clues[7],
+            (chose["angie", "arrabiata_sauce"] & ~chose["elisa", "arrabiata_sauce"]) | (~chose["angie", "arrabiata_sauce"] & chose["elisa", "arrabiata_sauce"])
+        )
+    )
+
+    clueTexts = [
+        "The person who ordered capellini paid less than the person who chose arrabiata sauce",
+        "The person who chose arrabiata sauce ordered farfalle",
+        "The person who ordered tagliolini paid less than the person who chose marinara sauce",
+        "The person who ordered tagliolini paid more than Angie",
+        "The person who ordered rotini is either the person who paid $8 more than Damon or the person who paid $8 less than Damon",
+        "Claudia did not choose puttanesca sauce",
+        "The person who ordered capellini is either Damon or Claudia",
+        "The person who chose arrabiata sauce is either Angie or Elisa"
+    ]
+
+    clues_cnf = cnf_to_pysat(to_cnf(clues))
+    bij_cnf = cnf_to_pysat(to_cnf(bij))
+    trans_cnf = cnf_to_pysat(to_cnf(trans))
+    # print(len(clues_cnf))
+
+    hard_clauses = [c for c in clues_cnf + bij_cnf + trans_cnf]
+    soft_clauses = []
+    soft_clauses += [[bv1.name + 1] for bv1 in bv_clues]
+    soft_clauses += [[bv1.name + 1]  for bv1 in bv_bij]
+    soft_clauses += [[bv1.name + 1]  for bv1 in bv_trans]
+
+    weights = {}
+    weights.update({bv.name + 1: 100 for bv in bv_clues})
+    weights.update({bv.name + 1: 60 for bv in bv_trans})
+    weights.update({bv.name + 1: 60 for bv in bv_bij})
+
+    explainable_facts = set()
+    bvRels = {}
+    for rel, relStr in zip(rels, ["chose", "paid", "ordered", "sauce_dollar", "sauce_pasta", "dollar_pasta"]):
+        rowNames = list(rel.df.index)
+        columnNames = list(rel.df.columns)
+        for r in rowNames:
+            for c in columnNames:
+                print(relStr, "row=",r, "col=", c, rel.df.at[r, c])
+                bvRels[rel.df.at[r, c].name + 1] = {"pred" : relStr.lower(), "subject" : r.lower(), "object": c.lower()}
+        for item in rel.df.values:
+            explainable_facts |= set(i.name+1 for i in item)
+
+
+    matching_table = {
+        'bvRel': bvRels,
+        'Transitivity constraint': [bv.name + 1 for bv in bv_trans],
+        'Bijectivity': [bv.name + 1 for bv in bv_bij],
+        'clues' : {
+            bv.name + 1: clueTexts[i] for i, bv in enumerate(bv_clues)
+        }
+    }
+
+    return hard_clauses, soft_clauses, weights, explainable_facts, matching_table
+
 
 def originProblemReify():
     """
